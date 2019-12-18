6af961a8bd132fb302943483978a40778308c10a
Andrei Barbu <andrei@0xab.com>
2019-12-18 00:36:06 -0500

    set index

--- docs/conf.py
+++ docs/conf.py
@@ -50,3 +50,12 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ['.rst', '.md', '.lhs']
+
+# The master toctree document.
+master_doc = 'index'
+
